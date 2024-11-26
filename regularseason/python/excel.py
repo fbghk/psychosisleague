cfa32295009ab27622e987bdcf49ed8625bd1807
fbghk <im706374@gmail.com>
2024-11-26 13:06:34 +0900

    Edit  | excel.py
    ※ 오류 없음. 투수 데이터만 추출 되도록 정제
    
    수정 내용:
    1. output_data 대신 data를 사용하여 re.sub 함수에 문자열을 전달.
    2. data는 이미 문자열이기 때문에 re.sub 함수에 적합.

--- regularseason/python/excel.py
+++ regularseason/python/excel.py
@@ -1,5 +1,4 @@
 import re
-import pandas as pd
 import json
 
 # JSON 파일 읽기
@@ -9,10 +8,8 @@ with open('data.json', 'r', encoding='utf-8') as file:
 # 첫 번째 객체의 "content" 값 추출
 data = json_data[0]['content']
 
-# content 값을 JSON 형태로 저장
-output_data = {"content": data}
-
 # 공백 및 불필요한 부분 제거하고 리스트로 변환
-cleaned_data = re.sub(r'[\t\n]+', ' ', output_data)
+cleaned_data = re.sub(r'[\t\n]+', ' ', data)
 
+# 결과 출력
 print(cleaned_data)
